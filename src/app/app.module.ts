cde894d8b031ca875572b6aed8c42e9016c01f35
Geovani Boton <geovani_bernardo92@hotmail.com>
2022-01-12 00:22:11 -0300

    remoção de imports inúteis

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameModule } from './components/game/game.module';
-import { HeroesSelectedComponent } from './components/heroes-selected/heroes-selected.component';
-import { CongratulationsComponent } from './components/congratulations/congratulations.component';
 
 @NgModule({
   declarations: [
