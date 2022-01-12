1eb05e3512baf3b7000109f8a89f16dd090d1ca8
Geovani Boton <geovani_bernardo92@hotmail.com>
2022-01-12 00:22:42 -0300

    remoção de import não usado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { TitleModule } from './components/title/title.module';
-import { HeroSelectionModule } from './components/hero-selection/hero-selection.module';
 import {  HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
